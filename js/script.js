816c65e940ec5b4e3b8ee905848cf000b8602dfc
Amy Evans <amyevans87@gmail.com>
2014-05-18 14:31:23 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$(".research").on('click', 'a', function() {
+		$.ajax('research.html', {
+			success: function(response) {
+				$('#details').html(response).fadeIn();
+			}
+		});
+	});
+	$(".education").on('click', 'a', function() {
+		$('.research').last().hide();
+		$('.presentations').last().hide();
+		$(this).closest('.container').find('.education').last().fadeIn();
+	});
+	$(".presentations").on('click', 'a', function() {
+		// event.preventDefault();
+		$('.research').last().hide();
+		$('.education').last().hide();
+		$(this).closest('.container').find('.presentations').last().fadeIn();
+	});
+});
+
