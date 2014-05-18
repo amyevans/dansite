0e2f11aebd82ad5a6535785fbbb5a53e16b1bfd2
Amy Evans <amyevans87@gmail.com>
2014-05-18 16:03:20 -0500

    Simplify AJAX

--- js/script.js
+++ js/script.js
@@ -16,3 +16,66 @@ $(document).ready(function() {
 	});
 });
 
+
+var details = {
+  init: function() {
+    $(".research").on('click', 'a', this.loadResearch);
+    $(".education").on('click', 'a', this.loadEducation);
+    $(".presentations").on('click', 'a', this.loadPresentations);
+  },
+  loadResearch: function() {
+    $.ajax('research.html', {
+          success: function(response) {
+               $('#details').html(response).fadeIn();
+          },
+          error: function(request, errorType, errorMessage) {
+               alert('Error: ' + errorType + ' with message: ' + errorMessage);
+          },
+          timeout: 3000,
+          beforeSend: function() {
+               $('#details').addClass('is-loading');
+          },
+          complete: function() {
+               $('#details').removeClass('is-loading');
+          }
+    });
+  },
+  loadEducation: function() {
+    $.ajax('education.html', {
+          success: function(response) {
+               $('#details').html(response).fadeIn();
+          },
+          error: function(request, errorType, errorMessage) {
+               alert('Error: ' + errorType + ' with message: ' + errorMessage);
+          },
+          timeout: 3000,
+          beforeSend: function() {
+               $('#details').addClass('is-loading');
+          },
+          complete: function() {
+               $('#details').removeClass('is-loading');
+          }
+    });
+  },
+  loadPresentations: function() {
+    $.ajax('presentations.html', {
+          success: function(response) {
+               $('#details').html(response).fadeIn();
+          },
+          error: function(request, errorType, errorMessage) {
+               alert('Error: ' + errorType + ' with message: ' + errorMessage);
+          },
+          timeout: 3000,
+          beforeSend: function() {
+               $('#details').addClass('is-loading');
+          },
+          complete: function() {
+               $('#details').removeClass('is-loading');
+          }
+    });
+  }  
+};
+$(document).ready(function() {
+  details.init();
+});
+
