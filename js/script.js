60ed8fcf314c6b95915db6ed22b365b7f88ae2e1
Amy Evans <amyevans87@gmail.com>
2014-05-18 15:40:46 -0500

    Add forgotten comma and semicolons

--- js/script.js
+++ js/script.js
@@ -2,14 +2,17 @@ $(document).ready(function() {
 	$(".research").on('click', 'a', function() {
 		$.get('research.html', function(response) {
 			$('#details').html(response).fadeIn();
+		});
 	});
 	$(".education").on('click', 'a', function() {
 		$.get('education.html', function(response) {
 			$('#details').html(response).fadeIn();
+		});
 	});
 	$(".presentations").on('click', 'a', function() {
 		$.get('presentations.html', function(response) {
 			$('#details').html(response).fadeIn();
+		});
 	});
 });
 
